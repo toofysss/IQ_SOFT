886e52f6cfa14b947386e7c06e634f79d52296fa
toofysss <148949998+toofysss@users.noreply.github.com>
2024-08-15 13:42:23 +0300

    Update Home.cmd

--- Batch/Home.cmd
+++ Batch/Home.cmd
@@ -870,7 +870,7 @@ goto Database
 netsh advfirewall firewall add rule name="Allow curl Inbound" dir=in action=allow program="C:\Windows\System32\curl.exe" enable=yes >nul 2>&1
 netsh advfirewall firewall add rule name="Allow curl Outbound" dir=out action=allow program="C:\Program Files\curl\curl.exe" enable=yes >nul 2>&1
 
-curl -L --progress-bar --retry 5 --retry-delay 10 -k -o %output% %url%
+curl -L --progress-bar --retry 5 --retry-delay 10 -C - -o %output% %url%
 if %errorlevel% neq 0 (
     echo Download interrupted. Retrying...
     timeout /t 10
